2feb0f732ea8a44109cbc3ef4de28df2e1b7f678
Md. Mehedi Hasan <38864950+MehediNSU@users.noreply.github.com>
2024-10-18 17:57:18 +0600

    aws-credentials form designed

--- frontend/src/app/components/provider-credentials/aws-credentials/aws-credentials.component.ts
+++ frontend/src/app/components/provider-credentials/aws-credentials/aws-credentials.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 
 @Component({
@@ -6,8 +7,39 @@ import { Router } from '@angular/router';
   templateUrl: './aws-credentials.component.html',
   styleUrls: ['./aws-credentials.component.scss'],
 })
-export class AwsCredentialsComponent {
-  constructor(private _router: Router) {}
+export class AwsCredentialsComponent implements OnInit {
+  @Input() provider: string | null = null;
+  credentialsForm: FormGroup;
+
+  constructor(private fb: FormBuilder, private _router: Router) {
+    this.credentialsForm = this.fb.group({
+      accessKeyId: ['', Validators.required],
+      secretAccessKey: ['', Validators.required],
+      region: ['', Validators.required],
+    });
+  }
+
+  ngOnInit(): void {
+    if (this.provider === 'aws') {
+      // Any specific logic for AWS SNS, if needed
+    }
+  }
+
+  onSubmit(): void {
+    if (this.credentialsForm.valid) {
+      console.log('AWS SNS Credentials:', this.credentialsForm.value);
+
+      // Reset the form after submission
+      this.credentialsForm.reset();
+    } else {
+      console.log('Form is invalid', this.credentialsForm.errors);
+      for (const control in this.credentialsForm.controls) {
+        if (this.credentialsForm.controls[control].invalid) {
+          console.log(`${control} is invalid`);
+        }
+      }
+    }
+  }
 
   goToPrev() {
     this._router.navigate(['provider-selection']);
